b6473b2f0385dbe44a6084bde7ed4d2cbd7467ee
Ernest Cheung <ernestcheung02@gmail.com>
2023-10-09 08:45:25 -0700

    Auto stash before revert of "Added ViewCoin components and tailwind-ify Navbar"

--- src/Components/ViewCoinComponents/TitleBox.js
+++ src/Components/ViewCoinComponents/TitleBox.js
@@ -1,7 +1,20 @@
 
-function TitleBox() {
+import copyLinkIcon from 'images/copy-link-icon.png'
+import { CopyLinkIcon, LinkPageRef, TitleSquare, TitleBoxWrapper  } from 'Pages/ViewCoin/ViewCoin.style'
+
+function TitleBox(props) {
+    const { name, homepage, symbol } = props
     return (
-        <div></div>
+        <TitleBoxWrapper>
+            <TitleSquare>
+                <h2 className='text-2xl'>{name}</h2>
+                <h2 className='text-gray-400'>({symbol})</h2>
+            </TitleSquare>
+            <LinkPageRef href={homepage} target="_blank" >
+                <CopyLinkIcon src={copyLinkIcon}  />
+                {homepage}
+            </LinkPageRef>
+        </TitleBoxWrapper>
     )
 }
 
---
+++ src/Pages/ViewCoin/ViewCoin.style.js
@@ -0,0 +1,88 @@
+
+export const MoreInfoList = tw.header`
+    text-md
+    font-semibold
+    flex
+    flex-row
+    justify-center
+    items-center
+`
+
+export const IfSmallerWindow = tw.div`
+    flex
+    flex-row
+    lg:flex-col
+    width-full
+    m-0
+    p-0
+`
+
+
+export const DescriptionBox = tw.div`
+    bg-primary
+    rounded-3xl
+    p-16
+    m-20
+    flex
+    flex-col
+`
+
+export const DescriptionText = tw.p`
+    text-white
+`
+
+export const ToggleDescriptionButton = tw.button`
+    text-green-400
+    mt-10
+`
+
+export const CopyLinkIcon = tw.img`
+    mr-5
+`
+
+export const LinkPageRef = tw.a`
+    bg-primary 
+    text-white 
+    flex 
+    flex-row 
+    justify-center 
+    items-center 
+    rounded-full 
+    h-10 
+    p-7 
+    mt-auto
+`
+
+export const TitleSquare = tw.div`
+    flex
+    flex-col
+    items-center
+    justify-center
+    w-40
+    h-40
+    bg-primary
+    rounded-3xl
+    m-20 
+    lg:m-0
+`
+export const LinksContainer = tw.div`
+
+`
+
+export const SectionWrapper = tw.div`
+    flex 
+    lg:flex-row 
+    flex-col 
+    mt-40 
+    justify-around 
+    items-center
+`
+
+export const TitleBoxWrapper = tw.div`
+    flex 
+    m-20 
+    lg:m-0 
+    flex-col 
+    h-80 
+    items-center
+`
