4b9c7803ce5375ec7bc9656b209bcd28ebf1ee94
Ernest Cheung <ernestcheung02@gmail.com>
2023-10-11 17:52:24 -0700

    tempadd

--- src/Components/ViewCoinComponents/TitleBox.js
+++ src/Components/ViewCoinComponents/TitleBox.js
@@ -1,7 +1,21 @@
+import copyLinkIcon from 'images/copy-link-icon.png'
+import { OpenLinkIcon, LinkPageBox, LinkPageRef, TitleSquare, TitleBoxWrapper  } from 'Pages/ViewCoin/ViewCoin.style'
 
-function TitleBox() {
+function TitleBox(props) {
+    const { name, homepage, symbol } = props
     return (
-        <div></div>
+        <TitleBoxWrapper>
+            <TitleSquare>
+                <h2 className='text-2xl'>{name}</h2>
+                <h2 className='text-gray-400'>({symbol})</h2>
+            </TitleSquare>
+            <LinkPageBox>
+                <LinkPageRef href={homepage} target="_blank" >
+                    <OpenLinkIcon src={copyLinkIcon}  />
+                </LinkPageRef>
+                {homepage}
+            </LinkPageBox>
+        </TitleBoxWrapper>
     )
 }
 
