6dc2ba82be42640cbc040f1ebf5fceb241d17148
Ernest Cheung <ernestcheung02@gmail.com>
2023-11-14 13:50:06 -0800

    added history and improved api calls

--- src/Components/AssetsComponent/Assets.js
+++ src/Components/AssetsComponent/Assets.js
@@ -2,17 +2,35 @@ import { AssetsWrapper } from "./Assets.style"
 import { useSelector } from 'react-redux'
 import EachAsset from "./EachAsset"
 import { useEffect, useState } from "react"
-import { getAllCoinsWithImages } from "helpers/getCoin"
+import { getAllCoinsWithImages, getHistoricalCoin } from "helpers/getCoin"
+import { current } from "@reduxjs/toolkit"
 
 
 function Assets() {
     const [allCoins, setAllCoins] = useState([])
+    const [allHistoryCoins, setAllHistoryCoins] = useState([])
     const currencyType = useSelector((state) => state.persist.currency)
     const purchasedCurrencies = useSelector((state) => state.persist.portfolio.currencies)
+
+    const boughtCurrencyIds = purchasedCurrencies.map((each) => each.id)
+
     useEffect(() => {
-        getAllCoinsWithImages(currencyType.currency).then((res) => setAllCoins(res))
+        // Get only the coins that we have purchased
+        getAllCoinsWithImages(currencyType.currency).then((res) => setAllCoins(res.filter((each) => boughtCurrencyIds.includes(each.id))))
+        // Get coins history that we have purchased
+        setAllHistoryCoins(purchasedCurrencies.map((each) => getHistoricalCoin(each, each.datePurchased, currencyType.currency).then((res) => res)))
     }, [currencyType.currency])
 
+    console.log(allHistoryCoins)
+    console.log(allCoins)
+
+    // redux only record id, name, priceWhenBought
+    // In assets:
+        // Make api call to get current data with redux id (done)
+        // Make api call to get historical data with redux id
+
+    
+    
 
     return (
         <AssetsWrapper>
