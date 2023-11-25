8cb3eaa2d12325157aff2a2c4e50eda1e7c1a2c3
Ernest Cheung <ernestcheung02@gmail.com>
2023-11-24 23:23:13 -0800

    temp com

---
+++ src/redux/index.js
@@ -0,0 +1,24 @@
+import { persistReducer, persistStore } from 'redux-persist'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage';
+// Portfolio
+import { portfolioSlice } from './portfolioSlice'
+import portfolioReducer from './portfolioSlice'
+import { currencySlice } from './currencySlice';
+import currencyReducer from './currencySlice'
+
+const rootReducer = combineReducers({currency: currencyReducer, portfolio: portfolioReducer})
+
+const persistedReducer = persistReducer({
+    key: 'root',
+    storage: storage,
+}, rootReducer)
+
+const store = configureStore({
+    reducer: {
+        persist: persistedReducer
+    }
+})
+
+export default store;
+export const persistor = persistStore(store)
