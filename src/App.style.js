8564902f742f10d36c1602095a14f1d385a1939c
Ernest Cheung <ernestcheung02@gmail.com>
2023-12-28 15:58:45 -0800

    Small styling changes

--- src/App.style.js
+++ src/App.style.js
@@ -14,7 +14,7 @@ export const Title = tw.h1`
 export const Background = styled.div`
     background: linear-gradient(rgba(31, 33, 40, 0.1) 0%, rgba(31, 33, 40, 1) 50%), linear-gradient(90deg, rgba(96,54,54,1) 0%, rgba(121,41,61,1) 11%, rgba(144,44,69,1) 20%, rgba(152,45,72,1) 27%, rgba(58,30,110,1) 51%, rgba(43,44,114,1) 61%, rgba(30,87,126,1) 74%, rgba(62,106,161,1) 90%, rgba(48,62,62,1) 100%);
     border-top: 0px black solid;
-    h-x
+    min-height: 150vh;
 `
 
 export const MidCol = tw.div`
@@ -27,5 +27,4 @@ export const MidRow = tw.div`
     flex
     flex-row
     items-center
-    h-x
 `
